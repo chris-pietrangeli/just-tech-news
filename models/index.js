faffa6a828e476127cbeff528b61b3902edf5b82
Chris Pietrangeli <chris.pietrangeli11@gmail.com>
2022-03-17 00:25:27 -0700

    add comments

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const Comment = require('./Comment');
 const Vote = require('./Vote');
 const User = require('./User');
 const Post = require('./Post');
@@ -40,8 +41,24 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
 
 
 
-module.exports = { User, Post, Vote };
+
+module.exports = { User, Post, Vote, Comment };
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/Users', userRoutes);
 router.use('/Posts', postRoutes);
+router.use('/comments', commentRoutes);
+
 
 module.exports = router;
